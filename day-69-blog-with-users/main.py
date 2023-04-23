7e6da3b623cae842d9b100b66cbc60e3c4d56271
yujiaohe <34307798+yujiaohe@users.noreply.github.com>
2023-04-23 22:05:56 +0800

    Update main.py

--- day-69-blog-with-users/main.py
+++ day-69-blog-with-users/main.py
@@ -9,7 +9,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
-from flask_ckeditor import CKEditor
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
